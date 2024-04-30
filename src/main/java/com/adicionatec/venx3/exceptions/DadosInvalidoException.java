cf55305e951ee434451be96d608c86a29dca2e8b
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-04-30 15:40:58 +0100

    Classes removed

--- src/main/java/com/adicionatec/venx3/exceptions/DadosInvalidoException.java
+++
@@ -1,9 +0,0 @@
-package com.adicionatec.venx3.exceptions;
-
-public class DadosInvalidoException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public DadosInvalidoException(String errorMessage) {
-        super(errorMessage);
-    }
-}
--- src/main/java/com/adicionatec/venx3/exceptions/UsuarioCadastradoException.java
+++
@@ -1,9 +0,0 @@
-package com.adicionatec.venx3.exceptions;
-
-public class UsuarioCadastradoException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public UsuarioCadastradoException(String email) {
-        super("Usuário com email " + email + " já está cadastrado");
-    }
-}
